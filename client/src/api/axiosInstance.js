9bdd72497d5d3e67e9202dcaee9b7984f5a22606
ayusha28 <97336700+ayusha28@users.noreply.github.com>
2024-11-21 21:35:02 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://teachwave.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
